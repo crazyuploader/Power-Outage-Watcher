5037579494a393f9134fdb141f38a8becf2072ff
Jugal Kishore <me@devjugal.com>
2025-07-03 19:35:08 +0530

    Restructure directory structure
    main.py
    parser.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ This is the main file for the project.
 
 __author__ = "Jugal Kishore <me@devjugal.com>"
 
-from parser import parse_mysore_power_outage
+
+from parsers.cesc_mysore import parse_mysore_power_outage
 
 # Global Variable(s)
 MYSORE_POWER_OUTAGE_URL = "https://cescmysore.karnataka.gov.in/new-page/Scheduled Power Interruption information 2025-26/en"
@@ -16,6 +17,8 @@ def main():
     """
     Main function to execute the script.
     """
+    print("Starting PowerOutageWatch script...")
+
     # Parse the Mysore Power Outage page
     outages = parse_mysore_power_outage(MYSORE_POWER_OUTAGE_URL)
 
